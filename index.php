c23449fc8e271c20ae4f9115da8b653a5c911411
rudyravelin <rudyravelin83@gmail.com>
2018-02-07 11:50:57 -0500

    journal1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title></title>
+</head>
+<body>
+	<div class="slider">
+		<figure>
+			<div class="slide">
+				<img src="images/slide_one.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_two.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_three.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_four.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_five.jpg">
+			</div>
+		</figure>
+	</div>
+</body>
+</html>
