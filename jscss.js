733a82ee79731700517432ea545d7e47f1f2b2a2
rudyravelin <rudyravelin83@gmail.com>
2018-02-09 19:01:27 -0500

    test

--- jscss.js
+++ jscss.js
@@ -1,24 +1,6 @@
+var input = document.querySelector('input')
+var footer = document.querySelector('footer')
 
-
-var zoomOne = document.getElementsByClassName('zoomPic')[0],
-    zoomOneBGSize = window.getComputedStyle(zoomOne).getPropertyValue('background-size'),
-    $zoomTwo = $('.zoomPic:eq(1)'),
-    zoomTwoBGSize = $zoomTwo.css('background-size');
-
-document.getElementsByClassName('zoom')[0].onclick = function() {
-  if(!zoomOne.classList.contains('zoom'))
-  {
-    zoomOne.classList.add('zoom');
-  } 
-}
-document.getElementsByClassName('pause')[0].onclick = function() {
-    var computedStyle = window.getComputedStyle(zoomOne),
-        backgroundSize = computedStyle.getPropertyValue('background-size');
-    zoomOne.style.backgroundSize = backgroundSize;
-    zoomOne.classList.remove('zoom');
-  }
-document.getElementsByClassName('zoomout')[0].onclick = function() {
-  zoomOne.classList.remove('zoom');
-  zoomOne.style.backgroundSize = zoomOneBGSize;
-}
-
+input.addEventListener('change', function(){
+  footer.style.setProperty('--footer-color', input.value)
+})
