13e84cb8540b4204e7b60d7e671a89dbe61a96a8
rudyravelin <rudyravelin83@gmail.com>
2018-04-03 12:49:12 -0400

    Topic4a

--- topic4.js
+++ topic4.js
@@ -7,9 +7,20 @@ btn.addEventListener("click", function() {
 	var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 ourRequest.onload = function(){
-	var ourData = JSON.parse(ourRequest.responseText);
+	if (ourRequest.status >=200 && ourRequest.status < 400) {
+		var ourData = JSON.parse(ourRequest.responseText);
 	renderHTML(ourData);
+} else{
+	console.log("We connected to the server, but it returned an error.");
+
+}
+	
 };
+
+	ourRequest.onerror = function() {
+		console.log("Connection error");
+	}
+
 	ourRequest.send();
 	pageCounter++;
 	if (pageCounter > 3) {
